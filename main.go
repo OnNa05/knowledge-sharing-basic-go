586f8143c13d2d1ebe37ed2918a79f806ada3f58
apirat tatsanawongwara <apirat.t05@gmail.com>
2023-03-18 09:00:56 +0700

    add authentication middleware

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/labstack/echo"
@@ -14,7 +15,6 @@ func LoggingMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		startTime := time.Now()
 
-		// Call the next handler in the chain
 		err := next(c)
 		if err != nil {
 			c.Error(err)
@@ -22,16 +22,42 @@ func LoggingMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 
 		// Log info
 		endTime := time.Now()
-		duration := endTime.Sub(startTime)
+		_ = endTime.Sub(startTime) // duration
 		clientIP := c.RealIP()
 		method := c.Request().Method
 		path := c.Path()
 		statusCode := c.Response().Status
 
 		fmt.Printf("[%s] %s %s %d %s\n", endTime.Format(time.RFC3339), clientIP, method, statusCode, path)
-		fmt.Printf("Request processing time: %s\n", duration)
-		
+		// fmt.Printf("Request processing time: %s\n", duration)
+
 		return nil
+	}
+}
+
+// add authentication middleware
+func AuthenticationMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		authHeader := c.Request().Header.Get("Authorization")
+
+		if authHeader == "" {
+			return echo.NewHTTPError(http.StatusUnauthorized, "missing Authorization header")
+		}
+
+		authParts := strings.Split(authHeader, " ")
+
+		if len(authParts) != 2 || strings.ToLower(authParts[0]) != "bearer" {
+			return echo.NewHTTPError(http.StatusUnauthorized, "invalid Authorization header format")
+		}
+
+		token := authParts[1]
+
+		// token validation logic
+		if token != "token" {
+			return echo.NewHTTPError(http.StatusUnauthorized, "invalid token")
+		}
+
+		return next(c)
 	}
 }
 
@@ -40,8 +66,14 @@ func main() {
 
 	e.Use(LoggingMiddleware)
 
+	e.Use(AuthenticationMiddleware)
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.GET("/protected", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Protected resource")
 	})
 
 	e.Logger.Fatal((e.Start(":1323")))
